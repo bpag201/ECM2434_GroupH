0804fdc91bd3e6f80c149ade9616631bfd0eb0df
Va1-HallA <56449583+Va1-HallA@users.noreply.github.com>
2021-02-06 18:01:03 +0000

    Update models.py

--- ECM2434/study_platform/models.py
+++ ECM2434/study_platform/models.py
@@ -1,2 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+
+class User(models.Model):
+    class Tiers(models.TextChoices):
+        STUDENT = "STU", _("Student")
+        STAFF = "STF", _("Staff")
+        ADMIN = "ADM", _("Admin")
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=320)
+    nickname = models.CharField(max_length=20)
+    course = models.IntegerField()
+    score = models.IntegerField()
+    resource = models.IntegerField()
+    avatar = models.ImageField()
+    user_tier = models.CharField(
+        max_length=3,
+        choices=Tiers.choices,
+        default=Tiers.STUDENT
+    )
