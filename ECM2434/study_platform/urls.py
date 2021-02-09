938bff8002b2a9786ef866d8779694ba41475a3a
bpag201 <bpag201@exeter.ac.uk>
2021-02-09 16:06:53 +0000

    Updated files
    Django now runs the webpage

--- ECM2434/study_platform/urls.py
+++ ECM2434/study_platform/urls.py
@@ -4,4 +4,6 @@ from . import forms
 
 app_name = 'study_platform'
 urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('login', views.home_view, name='login')
 ]
--- ECM2434/study_platform/views.py
+++ ECM2434/study_platform/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home_view(request):
+    if request.method == 'POST':
+        print("a")
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print("b")
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('profile')
+    else:
+        print("c")
+        form = UserCreationForm()
+    print("d")
+    return render(request, "register.html")
