1d60ff970c29488dd0a6847c20959420733c180b
bpag201 <bpag201@exeter.ac.uk>
2021-02-10 13:47:02 +0000

    More new login stuff

--- ECM2434/study_platform/views.py
+++ ECM2434/study_platform/views.py
@@ -1,21 +1,26 @@
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 def register_view(request):
     if request.method == 'POST':
         print("a")
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(data=request.POST)
+        print(request.POST)
+        print(form.is_valid())
         if form.is_valid():
             print("b")
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
+            raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('profile')
+            return redirect('')
     else:
         print("c")
         form = UserCreationForm()
@@ -24,8 +29,26 @@ def register_view(request):
 
 
 def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                redirect('')
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
     form = AuthenticationForm()
     return render(request, "login.html", {'form':form})
 
+<<<<<<< Updated upstream
 def home_view(request):
     return render(request, "homepage.html")
+=======
+User = get_user_model()
+>>>>>>> Stashed changes
