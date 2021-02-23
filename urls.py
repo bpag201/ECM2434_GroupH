926b0201dd905bf8e414321942f95020df1f9ae4
Va1-HallA <56449583+Va1-HallA@users.noreply.github.com>
2021-02-23 16:40:55 +0000

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from . import forms
+
+app_name = 'study_platform'
+urlpatterns = [
+    path('', views.home_view, name='home'),           #Will be changed to home page when made
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile_view, name='profile'),
+    path('shop/', views.shop, name='shop'),
+]
