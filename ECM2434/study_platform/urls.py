7587014ecf44b343d0a1e7f3a0df56f751bae813
bpag201 <56762556+bpag201@users.noreply.github.com>
2021-02-24 10:40:49 +0000

    Update urls.py
    Added comments

--- ECM2434/study_platform/urls.py
+++ ECM2434/study_platform/urls.py
@@ -4,9 +4,9 @@ from . import forms
 
 app_name = 'study_platform'
 urlpatterns = [
-    path('', views.home_view, name='home'),           #Will be changed to home page when made
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('profile/', views.profile_view, name='profile'),
-    path('shop/', views.shop, name='shop'),
+    path('', views.home_view, name='home'),                    # Will be changed to home page when made
+    path('register/', views.register_view, name='register'),   # For the registration page
+    path('login/', views.login_view, name='login'),            # For the login page
+    path('profile/', views.profile_view, name='profile'),      # For the profile page
+    path('shop/', views.shop, name='shop'),                    # For the shop page
 ]
