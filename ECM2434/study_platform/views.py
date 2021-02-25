663aa8d81d134da0ff7d32e51c95be26a5e7284e
bpag201 <bpag201@exeter.ac.uk>
2021-02-25 00:53:14 +0000

    Removed unnecessary print statements

--- ECM2434/study_platform/views.py
+++ ECM2434/study_platform/views.py
@@ -37,27 +37,16 @@ from django.contrib.auth import get_user_model
 '''
 def register_view(request):
     if request.method == 'POST':
-        print("a")
         form = UserCreationForm(data=request.POST)
-        print(request.POST)
-        print(form.is_valid())
         if form.is_valid():
-            print("b")
             form.save()
-            print("save")
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            print(username)
-            print(raw_password)
             user = authenticate(username=username, password=raw_password)
-            print("authenticated")
             login(request, user)
-            print("logged in")
             return redirect('/profile')
     else:
-        print("c")
         form = UserCreationForm()
-    print("d")
     return render(request, "register.html", {'form':form})
 
 '''
@@ -68,20 +57,16 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
-            print("form valid")
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                print("logged in")
                 #messages.info(request, f"You are now logged in as {username}")
-                print("blast off")
                 return redirect('/profile')
             else:
                 messages.error(request, "Invalid username or password")
         else:
-            print("form invalid")
             messages.error(request, "Invalid username or password")
 
     form = AuthenticationForm()
@@ -103,6 +88,8 @@ def profile_view(request):
     output: see pars
     """
 
+    # This code is commented as it currently doesn't work
+    
     # user_login_name = request.session['user_login_name']
     # we have to decide another credential for login since real name can be duplicated
     # cur_user = get_object_or_404(User, usename=user_login_name)
