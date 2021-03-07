3fe2d924627c01ccb1c24aeb4f92f4757fb9164e
bpag201 <bpag201@exeter.ac.uk>
2021-03-07 20:15:51 +0000

    Update resources.py
    Added get and set functions for the user's resources

--- ECM2434/study_platform/resources.py
+++ ECM2434/study_platform/resources.py
@@ -1,1 +1,26 @@
-def set_resource(resource):
+from .models import UserProfile
+from django.contrib.auth.models import User
+
+'''
+    Sets the given user's resources (currency) to the given value
+'''
+def set_resource(resource, username):
+    UserProfile.objects
+    User.objects
+
+    user = UserProfile(user=User(username=username))
+
+    user.resource = resource
+    user.save()
+
+'''
+    Get's the given user's resources
+'''
+def get_resource(username):
+    UserProfile.objects
+    User.objects
+
+    user = UserProfile(user=User(username=username))
+
+    resource = user.resource
+    return resource
