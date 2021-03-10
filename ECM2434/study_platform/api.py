b514afc18e04e2556d83800ca7ca5ebdb1273fa9
Harry813 <wangaoharry@hotmail.com>
2021-03-10 03:36:10 +0000

    Update api.py
    - Added gold_modify func
    - Added item_modify func

--- ECM2434/study_platform/api.py
+++ ECM2434/study_platform/api.py
@@ -1,18 +1,18 @@
 import logging
 import re
 import uuid
+import json
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.contrib.auth.decorators import login_required
 
-from groupH.models import Collection, Card, Comment, Blog, Option, User
-# from study_platform.models import Collection, Card, Comment, Blog, Option
+from .models import *
 
 
 logging.basicConfig(format='[%(asctime)s] - %(levelname)s: %(message)s',
                     level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
 
-# Done
 def reraise(exc_type, exc_value, exc_traceback=None):
     """
     Raise an exception, used to add information/message to a raised error
@@ -24,7 +24,6 @@ def reraise(exc_type, exc_value, exc_traceback=None):
     raise exc_value
 
 
-# Done
 def id_style_check(i):
     """
     check the style of id
@@ -38,7 +37,6 @@ def id_style_check(i):
     return len(re.findall("[0-9a-fA-F]", i.replace('-', ""))) == 32
 
 
-# Done
 def get_all_cards(collection):
     """
     Get a list of all cards in a specified collection
@@ -81,7 +79,6 @@ def get_all_cards(collection):
         raise TypeError("[ERROR] Invalid Input")
 
 
-# Done
 def get_options(card):
     """
     Get all options of a specified card.
@@ -124,7 +121,6 @@ def get_options(card):
         raise TypeError("[ERROR] Invalid Input")
 
 
-# Done
 def get_options_list(card_list):
     """
     get all options in a list of card
@@ -142,7 +138,7 @@ def get_options_list(card_list):
     return result
 
 
-# Done
+@login_required
 def add_card2coll(card, collection):
     """
     Insert a card to collection
@@ -209,7 +205,6 @@ def add_card2coll(card, collection):
     return s
 
 
-# Done
 def get_next_comt(father_id):
     """
     :param father_id: a 32/36-bit length id represented an instance of father_type
@@ -250,7 +245,6 @@ def get_next_comt(father_id):
         reraise(type(e), type(e)("[ERROR] " + msg))
 
 
-# Done
 def get_all_comt(father_id, amount=0):
     """
     get comments by a enter a ID.
@@ -331,6 +325,7 @@ def get_card_by_tag(*tags):
     return result
 
 
+@login_required
 def add_tags_to_Card(card, *tags):
     """
     add one or more tag to a specified card
@@ -371,7 +366,21 @@ def add_tags_to_Card(card, *tags):
             raise TypeError("[ERROR] " + msg)
 
 
+@login_required
 def remove_tags_from_Card(card, *tags):
+    """
+    Remove tag(s) from a card, **will not** raise an exception if tag(s) not exist
+
+    :param card: an instance of Card or a string represent Card ID
+    :type card: Card | str
+
+    :param tags: String(s) of tags
+    :type tags: str
+
+    :exception ObjectDoesNotExist: raises when no record found
+    :exception MultipleObjectsReturned: raises when multiple records returned on one ID
+    """
+
     if isinstance(card, Card):
         card.card_tags.remove(tags)
     elif isinstance(card, str):
@@ -393,3 +402,201 @@ def remove_tags_from_Card(card, *tags):
             msg = "Invalid ID, ID-{}".format(card)
             logging.info(msg)
             raise TypeError("[ERROR] " + msg)
+
+
+def get_rank(target):
+    """
+    get the corresponding rank title of a user according his/her score
+
+    :param target: Can use User or Profile instance as input, also allow integers
+    :type target: User | Profile | int
+
+    :return: will return a string represent
+    :rtype: str
+
+    :exception TypeError: raises when the input is not any of the above three classes
+    :exception FileNotFoundError: raises when rank.json not exists
+    :exception ObjectDoesNotExist: raises when no record found
+    :exception MultipleObjectsReturned: raises when multiple records returned on one instance
+    """
+
+    def rank(score):
+        try:
+            with open("groupH/rank.json") as f:
+                rank_dict = json.load(f)
+            for con in rank_dict:
+                if rank_dict[con]['LowerLimit'] < score <= rank_dict[con]['UpperLimit']:
+                    return rank_dict[con]['title']
+        except FileNotFoundError as e:
+            msg = "Important file(s) is missing, please double confirm the file(s) path and restart the server!\n" \
+                  "  - rank.json"
+            logging.critical(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+
+    if isinstance(target, User):
+        try:
+            return rank(Profile.objects.get(user=User).score)
+        except ObjectDoesNotExist as e:
+            msg = "0 records found in table:Profile with User-{}".format(target.username)
+            logging.info(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+        except MultipleObjectsReturned as e:
+            msg = "Multiple record found in table:Profile with User-{}".format(target.username)
+            logging.warning(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+    elif isinstance(target, Profile):
+        return rank(Profile.score)
+    elif isinstance(target, int):
+        return rank(target)
+    else:
+        msg = "Invalid input-{}".format(target)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+
+
+def get_profile(user):
+    """
+    Get the profile of a user
+    :param user: an instance of User
+    :type user: User
+
+    :return: The profile of the user
+    :rtype: Profile
+
+    :exception TypeError: raises when the input is not any of the above three classes
+    :exception ObjectDoesNotExist: raises when no record found
+    :exception MultipleObjectsReturned: raises when multiple records returned on one instance
+    """
+    if isinstance(user, User):
+        try:
+            profile = Profile.objects.get(user=user)
+            return profile
+        except ObjectDoesNotExist as e:
+            msg = "0 records found in table:Profile with User-{}".format(user.username)
+            logging.info(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+        except MultipleObjectsReturned as e:
+            msg = "Multiple record found in table:Profile with User-{}".format(user.username)
+            logging.warning(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+    else:
+        msg = "Invalid input-{}".format(user)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+
+
+def gold_modify(amount, user, description):
+    # todo: 完善documentation
+    """
+    modify the gold amount in a user profile
+
+    :param amount: amount of gold to be modified
+    :type amount: int
+
+    :param user: target user
+    :type user: User
+
+    :param description: reason of modification
+    :type description: str
+
+    :return: the updated profile
+    :rtype: Profile
+    """
+    if not isinstance(amount, int):
+        msg = "Invalid input-{}".format(amount)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+    elif not isinstance(description, str):
+        msg = "Invalid input-{}".format(description)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+    elif not isinstance(user, User):
+        msg = "Invalid input-{}".format(user)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+    else:
+        try:
+            profile = Profile.objects.get(user)
+            profile.gold_modify(amount)
+            return profile
+        except ObjectDoesNotExist as e:
+            msg = "0 records found in table:Profile with User-{}".format(user.username)
+            logging.info(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+        except MultipleObjectsReturned as e:
+            msg = "Multiple record found in table:Profile with User-{}".format(user.username)
+            logging.warning(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+
+
+def get_all_item(target):
+    # todo: 完善documentation
+    """
+
+    :param target:
+    :type target:
+    :return:
+    :rtype:
+    """
+    if isinstance(target, User):
+        try:
+            profile = Profile.objects.get(user=target)
+            return list(profile.resources.all())
+        except ObjectDoesNotExist as e:
+            msg = "0 records found in table:Profile with User-{}".format(target)
+            logging.info(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+        except MultipleObjectsReturned as e:
+            msg = "Multiple record found in table:Profile with User-{}".format(target)
+            logging.warning(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+    else:
+        msg = "Invalid input-{}".format(target)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+
+
+def modify_item(item, target, amount=1):
+    # todo: 完善documentation
+    if not isinstance(item, Item):
+        msg = "Invalid input-{}".format(item)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
+
+    if isinstance(target, User):
+        try:
+            target = Profile.objects.get(user=target)
+        except ObjectDoesNotExist as e:
+            msg = "0 records found in table:Profile with User-{}".format(target.username)
+            logging.info(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+        except MultipleObjectsReturned as e:
+            msg = "Multiple record found in table:Profile with User-{}".format(target.username)
+            logging.warning(msg)
+            reraise(type(e), type(e)("[ERROR] " + msg))
+
+    if isinstance(target, Profile):
+        if item in list(target.resources.all()):
+            rec = User_item.objects.get(item=item, profile=target)
+            if amount > 0:
+                rec.amount += amount
+                rec.save()
+            elif amount == 0:
+                msg = "amount cannot be 0"
+                logging.info(msg)
+                raise TypeError(msg)
+            elif amount < 0:
+                if rec.amount > abs(amount):
+                    rec.amount += amount
+                    rec.save()
+                elif rec.amount == abs(amount):
+                    rec.delete()
+                else:
+                    raise IndexError("Negative item amount generated")
+        else:
+            User_item.objects.create(item=item, profile=target)
+
+    else:
+        msg = "Invalid input-{}".format(item)
+        logging.info(msg)
+        raise TypeError("[ERROR] " + msg)
